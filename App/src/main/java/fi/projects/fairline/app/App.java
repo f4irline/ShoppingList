5e0ff0afd3ea4b1e96c8fc3c8a111ec513434b54
Tommi Lepola <lepola.tommi@gmail.com>
2018-11-06 14:00:07 +0200

    App: main(): Remove test print line.

--- App/src/main/java/fi/projects/fairline/app/App.java
+++ App/src/main/java/fi/projects/fairline/app/App.java
@@ -4,7 +4,6 @@ import fi.projects.fairline.ezparser.EzParser;
 public class App {
     public static void main(String[] args) {
         System.out.println("App started.");
-        System.out.println("Test");
         EzParser ezParser = new EzParser();
     }
 }
