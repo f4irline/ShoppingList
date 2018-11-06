a576f3efaf1edf0af81e2d244d32bff6f21eda8d
Tommi Lepola <lepola.tommi@gmail.com>
2018-11-06 13:56:31 +0200

    App: main(): Add print

--- App/src/main/java/fi/projects/fairline/app/App.java
+++ App/src/main/java/fi/projects/fairline/app/App.java
@@ -4,6 +4,7 @@ import fi.projects.fairline.ezparser.EzParser;
 public class App {
     public static void main(String[] args) {
         System.out.println("App started.");
+        System.out.println("Test");
         EzParser ezParser = new EzParser();
     }
 }
