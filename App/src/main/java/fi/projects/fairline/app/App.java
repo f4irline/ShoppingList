c13ea8cf918878e7bb720bce775216b16dc354e6
Tommi Lepola <lepola.tommi@gmail.com>
2018-11-06 13:12:52 +0200

    Init commit

---
+++ App/src/main/java/fi/projects/fairline/app/App.java
@@ -0,0 +1,9 @@
+package fi.projects.fairline.app;
+import fi.projects.fairline.ezparser.EzParser;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("App started.");
+        EzParser ezParser = new EzParser();
+    }
+}
---
+++ EzParser/src/main/java/fi/projects/fairline/ezparser/EzParser.java
@@ -0,0 +1,7 @@
+package fi.projects.fairline.ezparser;
+
+public class EzParser {
+    public EzParser () {
+        System.out.println("EzParser Initialized");
+    }
+}
