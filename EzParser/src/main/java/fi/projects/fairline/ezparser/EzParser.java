26640f022dd8890f3ec04c8c5bfbd5be72142d88
Tommi Lepola <lepola.tommi@gmail.com>
2018-11-07 09:27:08 +0200

    EzParser: Add createJSONFile() method. Creates a new JSON to resources.

--- EzParser/src/main/java/fi/projects/fairline/ezparser/EzParser.java
+++ EzParser/src/main/java/fi/projects/fairline/ezparser/EzParser.java
@@ -1,7 +1,30 @@
 package fi.projects.fairline.ezparser;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class EzParser {
+
+    private File jsonFile;
+    private FileWriter jsonWriter;
+
     public EzParser () {
+        createJSONFile();
         System.out.println("EzParser Initialized");
+    }
+
+    private void createJSONFile() {
+        try {
+            jsonFile = new File("EzParser/src/resources/data.json");
+            jsonFile.createNewFile();
+
+            jsonWriter = new FileWriter(jsonFile);
+            jsonWriter.write("Test");
+            jsonWriter.flush();
+    
+            jsonWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
